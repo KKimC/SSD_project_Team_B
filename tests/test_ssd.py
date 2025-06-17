e81fb8b2a436b923bfaab7b442be411e0e0dd45e
jinho <zazz0907@naver.com>
2025-06-17 15:39:37 +0900

    [test] ssd.py의 pass test case 추가

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -1,1 +1,19 @@
 import pytest
+
+def test_ssd_객체_선언_후_처음_read할때_0이_반환되는가():
+    pass
+
+def test_read가_output에_제대로_된_값을_전달하는가():
+    pass
+
+def test_read가_제대로_된_값을_리턴하는가():
+    pass
+
+def test_write시_file_manager의_patch가_호출되는가():
+    pass
+
+def test_write시_file_manager의_print_ssd_output에_제대로_된_값이_들어가는가():
+    pass
+
+def test_write시_nand에_제대로_된_값이_들어가는가():
+    pass
