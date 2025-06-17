c02df2dc57f6fde5a17c03f98e5957f90a933f00
jinho <zazz0907@naver.com>
2025-06-17 16:49:14 +0900

    [test] update argument for select_file_manager

--- src/ssd.py
+++ src/ssd.py
@@ -6,8 +6,8 @@ class SSD():
         self.nand = [ 0 for _ in range(100) ]
         self.ssd_file_manager = SSDFileManager()
 
-    def select_file_manager(self):
-        self.ssd_file_manager = SSDFileManager()
+    def select_file_manager(self, file_manager):
+        self.ssd_file_manager = file_manager
 
     def read(self, address):
         return self.nand[address]
