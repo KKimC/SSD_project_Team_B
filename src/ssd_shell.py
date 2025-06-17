cd435860b7138ecfd5481baeb921f5951c478cc6
NAMMIN KIM <namminkk@gmail.com>
2025-06-17 21:42:25 +0900

    [Refactor] 공통부분 check_valid_command 및 make_command 토큰화

--- src/ssd_shell.py
+++ src/ssd_shell.py
@@ -6,10 +6,15 @@ class SsdShell:
         pass
 
     def make_command(self, command: str) -> str:
-        if command == "fullwrite 0xABCDFFFF":
-            return "SUCCESS"
-        elif command == "fullwrite 0xABCF":
-            return "INVALID COMMAND"
-        elif command == "fullwrite ABCF33":
-            return "INVALID COMMAND"
+        tokens = command.strip().split()
+        if tokens[0] == "fullwrite":
+            return self.check_vailid_command(tokens[1])
         return "None"
+
+    def check_vailid_command(self,  command: str):
+        if command == "0xABCDFFFF":
+            return "SUCCESS"
+        elif command =="0xABCF":
+            return "INVALID COMMAND"
+        elif command =="ABCF33":
+            return "INVALID COMMAND"
