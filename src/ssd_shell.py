6631fd8baef0dbcc45349b0872164a045d92ad58
taeyun <t109.kang@samsung.com>
2025-06-18 10:35:03 +0900

    [Feature] tdd-green: WRITE명령어-Value 값이 유효하지 않는 경우
    - A~F, 0-9이외의 문자가 존재할 때
    - 앞에 0x가 존재해야됨.

--- src/ssd_shell.py
+++ src/ssd_shell.py
@@ -1,3 +1,6 @@
+import re
+
+
 class SsdShell:
     def __init__(self):
         pass
@@ -14,13 +17,16 @@ class SsdShell:
             return
 
         param1, param2 = command_list[1:]
-        if int(param1) < 0 or int(param1) > 99:
-            print("INVALID COMMAND")
-            return
         if type(param1) != int:
             print("INVALID COMMAND")
             return
+        if int(param1) < 0 or int(param1) > 99:
+            print("INVALID COMMAND")
+            return
 
+        if not self._is_valid_8char_hex(param2):
+            print("INVALID COMMAND")
+            return
         print("[Write] Done")
 
     def make_command(self) -> str:
@@ -36,3 +42,6 @@ class SsdShell:
 
     def is_invalid_read_command(self, command: str):
         return True
+
+    def _is_valid_8char_hex(self, write_value_str: str) -> bool:
+        return bool(re.fullmatch(r"0x[0-9a-fA-F]{8}", write_value_str))
