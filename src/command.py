1a61fcd7fb79b77983ec0c9ca72a190d77f2b033
taeyun <t109.kang@samsung.com>
2025-06-18 13:10:07 +0900

    [Refactor] 각 기능별 개발한 것 병합한 뒤, 코드 리팩터링
    - Command 패턴을 사용
    - 현재를 Receiver 없이, Invoker 와 Command 만 존재함. (Invoker는 SsdShell)

---
+++ src/command.py
@@ -0,0 +1,78 @@
+import re
+from typing import List
+from abc import abstractmethod, ABC
+
+
+class Command(ABC):
+    def __init__(self, args: List[str]):
+        self.args = args
+
+    @abstractmethod
+    def is_valid(self) -> bool: ...
+
+    @abstractmethod
+    def execute(self): ...
+
+    def _is_valid_8char_hex(self, write_value_str: str) -> bool:
+        return bool(re.fullmatch(r"0x[0-9a-fA-F]{8}", write_value_str))
+
+    def _is_valid_lba(self, value: str) -> bool:
+        try:
+            num = int(value)
+            return 0 <= num <= 99
+        except ValueError:
+            return False
+
+
+class WriteCommand(Command):
+
+    def __init__(self, args: List[str]):
+        super().__init__(args)
+
+    def is_valid(self) -> bool:
+        if len(self.args) != 3:
+            return False
+        lba_address, write_value = self.args[1:]
+        return self._is_valid_lba(lba_address) and self._is_valid_8char_hex(write_value)
+
+    def execute(self):
+        print("[Write] Done")
+
+
+class ReadCommand(Command):
+    def __init__(self, args: List[str]):
+        super().__init__(args)
+
+    def is_valid(self) -> bool:
+        if len(self.args) < 2:
+            return False
+
+        return self._is_valid_lba(self.args[1])
+
+    def execute(self):
+        print("[Read] Done")
+
+
+class FullReadCommand(Command):
+    def is_valid(self) -> bool:
+        pass
+
+    def execute(self):
+        list_cmds = self._make_cmds_for_fullread()
+        for i in range(100):
+            print("0x00000000")
+
+    def _make_cmds_for_fullread(self):
+        list_cmds = []
+        for i in range(100):
+            list_cmds.append(f"ssd.py R {i}")
+        return list_cmds
+
+
+class FullWriteCommand(Command):
+
+    def is_valid(self) -> bool:
+        return False
+
+    def execute(self):
+        print("[Write] Done")
--- src/ssd_shell.py
+++ src/ssd_shell.py
@@ -1,10 +1,20 @@
 import re
 
+from src.command import WriteCommand, ReadCommand, FullReadCommand, FullWriteCommand
+
 INVALID_COMMAND = "INVALID COMMAND"
 
+
 class SsdShell:
+    COMMAND_MAP = {
+        "read": ReadCommand,
+        "write": WriteCommand,
+        "fullread": FullReadCommand,
+        "fullwrite": FullWriteCommand,
+    }
+
     def __init__(self):
-        pass
+        self.validator = {}
 
     def _make_cmds_for_fullread(self):
         list_cmds = []
@@ -13,83 +23,25 @@ class SsdShell:
         return list_cmds
 
     def run(self):
-        command = self.make_command()
-        command_list = command.split()
+        command_str = self.make_command()
+        command_list = command_str.split()
         if not command_list:
             print(INVALID_COMMAND)
             return
 
         command_type = command_list[0]
-
-        if command_type not in ["read", "write", "fullwrite", "fullread"]:
+        command_class = self.COMMAND_MAP.get(command_type)
+        if not command_class:
             print(INVALID_COMMAND)
             return
-          
-        if command_type == "fullread":
-            list_cmds = self._make_cmds_for_fullread()
-            for i in range(100):
-                print("0x00000000")
-            return
-          
-        if command_type == "fullwrite":
-            if self.is_invalid_fullwrite_command(command):
-                print("INVALID COMMAND")
-                return
-            else:
-                # ssd.py fullwrite 0x12345678 호출부
-                print("[Write] Done")
-                return
 
-        if command_type == "read":
-            if self.is_invalid_read_command(command_list):
-                print(INVALID_COMMAND)
-                return
-              
-        elif command_type == "write":
-            if not self._validate_write(command_list):
-                print(INVALID_COMMAND)
-                return
-            print("[Write] Done")
+        command = command_class(args=command_list)
+        if not command.is_valid():
+            print(INVALID_COMMAND)
+            return
+
+        command.execute()
 
     def make_command(self) -> str or list[str]:
         command = input("Shell> ")
         return command
-
-    def real_full_read(self):
-        for i in range(100):
-            self.real_read()
-
-    def real_full_write(self):
-        for i in range(100):
-            self.real_write()
-
-    def is_invalid_fullwrite_command(self, command: str):
-        return True
-      
-    def is_invalid_read_command(self, command: list):
-        if len(command) < 2:
-            return True
-
-        LBA = command[1]
-        if not LBA.isdigit():
-            return True
-
-        LBA = int(command[1])
-        if LBA > 99 or LBA < 0:
-            return True
-
-        return False
-    def _validate_write(self, args):
-        if len(args) != 3:
-            return False
-        return self._is_valid_lba(args[1]) and self._is_valid_8char_hex(args[2])
-
-    def _is_valid_lba(self, value):
-        try:
-            num = int(value)
-            return 0 <= num <= 99
-        except ValueError:
-            return False
-
-    def _is_valid_8char_hex(self, write_value_str: str) -> bool:
-        return bool(re.fullmatch(r"0x[0-9a-fA-F]{8}", write_value_str))
