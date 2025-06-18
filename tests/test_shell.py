237285de75cff2bd84d447dc73c78cdfc0314eb4
Kiwoong <11101010@naver.com>
2025-06-18 13:10:35 +0900

    [refactor] extract common things
    tests/test_shell.py: 리다이렉션, SsdShell 인스턴스화 공통부로 뺌

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -6,137 +6,101 @@ from pytest_mock import MockerFixture
 
 from src.ssd_shell import SsdShell
 
+INVALID_COMMAND = "INVALID COMMAND"
 
-def test_READ_명령어유효성검사_유효하지않은명령어(mocker: MockerFixture):
+
+def _do_run_and_get_result_from_buffer(shell):
+    original_stdout = sys.stdout
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    # Act
+    shell.run()
+    sys.stdout = original_stdout
+    return captured_output.getvalue()
+
+
+@pytest.fixture
+def shell():
+    return SsdShell()
+
+
+def test_READ_명령어유효성검사_유효하지않은명령어(mocker: MockerFixture, shell):
     wrong_input_command = ["reead 3"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_명령어유효성검사_누락(mocker: MockerFixture):
+def test_READ_명령어유효성검사_누락(mocker: MockerFixture, shell):
     wrong_input_command = ["3"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_LBA유효성검사_누락(mocker: MockerFixture):
+def test_READ_LBA유효성검사_누락(mocker: MockerFixture, shell):
     wrong_input_command = ["read"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_LBA유효성검사_100초과(mocker: MockerFixture):
+def test_READ_LBA유효성검사_100초과(mocker: MockerFixture, shell):
     wrong_input_command = ["read 999"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_LBA유효성검사_음수(mocker: MockerFixture):
+def test_READ_LBA유효성검사_음수(mocker: MockerFixture, shell):
     wrong_input_command = ["read -1"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_LBA유효성검사_정수가아님(mocker: MockerFixture):
+def test_READ_LBA유효성검사_정수가아님(mocker: MockerFixture, shell):
     wrong_input_command = ["read abc"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_READ_SSD에서_값을_읽어오는가(mocker: MockerFixture):
+def test_READ_SSD에서_값을_읽어오는가(mocker: MockerFixture, shell):
     pass
 
 
-def test_READ_Console에_값을_출력하는가(mocker: MockerFixture):
+def test_READ_Console에_값을_출력하는가(mocker: MockerFixture, shell):
     # ex) Shell> read 3
     # [READ] LBA 03 : 0x00000000
     pass
 
 
-def test_READ_비어있는_LBA에서_읽은값이_0x00000000인가(mocker: MockerFixture):
+def test_READ_비어있는_LBA에서_읽은값이_0x00000000인가(mocker: MockerFixture, shell):
     # ex) Shell> read 3
     # 0x00000000
     pass
 
 
-def test_READ_txt파일이_없을때_읽은값이_0x00000000인가(mocker: MockerFixture):
+def test_READ_txt파일이_없을때_읽은값이_0x00000000인가(mocker: MockerFixture, shell):
     # ex) Shell> read 3
     # 0x00000000
     pass
 
 
-def test_READ_정상적인_값을_출력하는가(mocker: MockerFixture):
+def test_READ_정상적인_값을_출력하는가(mocker: MockerFixture, shell):
     # ex) Shell> read 3
     # 0x00000000
     pass
@@ -155,86 +119,53 @@ def test_WRITE명령어_시스템콜명령어를잘만드는가():
     pass
 
 
-def test_WRITE명령어_누락된인자(mocker: MockerFixture):
+def test_WRITE명령어_누락된인자(mocker: MockerFixture, shell):
     # Arrange
     wrong_input_command = ["write", "write 0xAAAABBBB", "write 3"]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_WRITE명령어_유효하지않은인자_INVALID_COMMAND(mocker: MockerFixture):
+def test_WRITE명령어_유효하지않은인자_INVALID_COMMAND(mocker: MockerFixture, shell):
     # ex) Shell> write -1 0xAAAABBBB
     # INVALID COMMAND
     # Arrange
     mocker.patch("builtins.input", return_value="write -1 0xAAAABBBB")
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 def test_WRITE명령어_유효하지않은인자_주소_100초과_INVALID_COMMAND(
-    mocker: MockerFixture,
+    mocker: MockerFixture, shell
 ):
     # ex) Shell> write 200 0xAAAABBBB
     # INVALID COMMAND
     # Arrange
     mocker.patch("builtins.input", return_value="write 200 0xAAAABBBB")
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 def test_WRITE명령어_유효하지않은인자_주소_정수가아님_INVALID_COMMAND(
-    mocker: MockerFixture,
+    mocker: MockerFixture, shell
 ):
     # ex) Shell> write ABC 0xAAAABBBB
     # INVALID COMMAND
     mocker.patch("builtins.input", return_value="write 1.5 0xAAAABBBB")
+    expected = INVALID_COMMAND
 
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
-def test_WRITE명령어_유효하지않은인자_값_INVALID_COMMAND(
-    mocker: MockerFixture,
-):
+
+def test_WRITE명령어_유효하지않은인자_값_INVALID_COMMAND(mocker: MockerFixture, shell):
     wrong_input_command = [
         "write 3 xxAAAABBBB",  # 0x가 없는경우
         "write 3 AAAABBBB12",
@@ -244,45 +175,29 @@ def test_WRITE명령어_유효하지않은인자_값_INVALID_COMMAND(
         "write 3 0x111111111111111111111",
     ]
     mocker.patch("builtins.input", side_effect=wrong_input_command)
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
-    expected = "INVALID COMMAND"
-    shell = SsdShell()
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-    # Assert
-    assert output.strip() == expected.strip()
+    expected = INVALID_COMMAND
+
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 def test_WRITE명령어_정상동작시_실제로파일에저장되는가():
     pass
 
 
-def test_WRITE명령어_정상인자_기대되는출력물을만드는가(mocker: MockerFixture):
+def test_WRITE명령어_정상인자_기대되는출력물을만드는가(mocker: MockerFixture, shell):
     # ex.
     # Shell> write 3 0xAAAABBBB
     # [Write] Done
     # Arrange
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="write 3 0xAAAABBBB")
-    shell = SsdShell()
-
     expected = "[Write] Done"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
 
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_HELP명령어_정상_기대되는출력(mocker):
+def test_HELP명령어_정상_기대되는출력():
     # ex.
     # Shell> help
     # 제작자: 김성현, 강태윤, 임동혁, 김기웅, 김남민, 정보람, 김민규
@@ -320,120 +235,70 @@ def test_공통_명령어_비정상인자_기대되는출력():
     pass
 
 
-def test_FULLWRITE명령어_정상_기대되는_출력(mocker: MockerFixture):
+def test_FULLWRITE명령어_정상_기대되는_출력(mocker: MockerFixture, shell):
     # ex.
     # Shell> fullwrite 0xABCDFFFF
     # 모든LBA에 값0xABCDFFF 가 적힌다
     # LBA 0~99까지 반복되며 run_ssd_command가 호출되는지 확인
-
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="fullwrite 0xAAAABBBB")
-    shell = SsdShell()
-
     expected = "[Write] Done"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
 
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_FULLWRITE명령어_비정상_짧은명령어_INVALID_COMMAND(mocker: MockerFixture):
+def test_FULLWRITE명령어_비정상_짧은명령어_INVALID_COMMAND(
+    mocker: MockerFixture, shell
+):
     # ex.
     # Shell> fullwrite 0xABCF
     # INVALID COMMAND
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="fullwrite 0xABCF")
-    shell = SsdShell()
+    expected = INVALID_COMMAND
 
-    expected = "INVALID COMMAND"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    print("prnt")
-    print(sys.stdout)
-    output = captured_output.getvalue()
-
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 def test_FULLWRITE명령어_비정상인자_0x없음_INVALID_COMMAND(
-    mocker: MockerFixture,
+    mocker: MockerFixture, shell
 ):
     # ex.
     # Shell> fullwrite ABCF33
     # INVALID COMMAND
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="fullwrite ABCF33")
-    shell = SsdShell()
+    expected = INVALID_COMMAND
 
-    expected = "INVALID COMMAND"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 def test_FULLWRITE명령어_비정상인자_특수문자_INVALID_COMMAND(
-    mocker: MockerFixture,
+    mocker: MockerFixture, shell
 ):
     # ex.
     # Shell> fullwrite 0x!@#$@@@
     # INVALID COMMAND
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="fullwrite 0x!@#$@@@")
-    shell = SsdShell()
+    expected = INVALID_COMMAND
 
-    expected = "INVALID COMMAND"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
-def test_FULLWRITE명령어_비정상인자_공백_INVALID_COMMAND(
-    mocker: MockerFixture,
-):
+def test_FULLWRITE명령어_비정상인자_공백_INVALID_COMMAND(mocker: MockerFixture, shell):
     # ex.
     # Shell> fullwrite
     # INVALID COMMAND
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
     mocker.patch("builtins.input", return_value="fullwrite")
-    shell = SsdShell()
+    expected = INVALID_COMMAND
 
-    expected = "INVALID COMMAND"
-    # Act
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-
-    # Assert
-    assert output.strip() == expected.strip()
+    # act and assert
+    assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
 
 
 # 이거 SSD.py 쪽이면 지워도?
-def test_FULLWRITE명령어_정상인자_실제로파일저장확인(
-    mocker: MockerFixture,
-):
+def test_FULLWRITE명령어_정상인자_실제로파일저장확인(mocker: MockerFixture, shell):
     # ex.
     # Shell> fullwrite 0xABCDFFFF
     # 모든LBA에 값0xABCDFFF 가 적힌다
@@ -452,20 +317,12 @@ def _make_100_reads():
     return list_cmds
 
 
-def test_FULLREAD명령어_정상인자_기대되는_출력(mocker: MockerFixture):
-    original_stdout = sys.stdout
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
+def test_FULLREAD명령어_정상인자_기대되는_출력(mocker: MockerFixture, shell):
     mocker.patch("builtins.input", return_value="fullread")
-
-    shell = SsdShell()
     expected_line_num = 100
-    shell.run()
-    sys.stdout = original_stdout
-    output = captured_output.getvalue()
-
-    arr_response = output.strip().splitlines()
+    arr_response = _do_run_and_get_result_from_buffer(shell).strip().splitlines()
     assert len(arr_response) == expected_line_num
+
     matched = True
     for response in arr_response:
         if _is_valid_8char_hex(response):
