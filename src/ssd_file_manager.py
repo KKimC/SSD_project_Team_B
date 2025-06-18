d7d826fe634f765ac5728b391d7b9b4ecdb41307
KKimC <nsn04116@gmail.com>
2025-06-18 09:54:37 +0900

    [feature] read_ssd_nand, patch_ssd_nand, print_ssd_output mathod in Add ssd_file_manager class

--- src/ssd_file_manager.py
+++ src/ssd_file_manager.py
@@ -10,14 +10,14 @@ class SSDFileManager():
             with open('ssd_output.txt', 'w', encoding='utf-8') as f:
                 f.write("")
 
+        self.patch_ssd_nand(["0x00000000" for _ in range(100)])
+
     def read_ssd_nand(self):
         with open('ssd_nand.txt', 'r', encoding='utf-8') as file:
             lines = file.readlines()
             return [line.rstrip('\n') for line in lines[:100]]
 
     def patch_ssd_nand(self, nand_list):
-        if len(nand_list) != 100:
-            raise ValueError("입력 리스트는 100개의 문자열을 포함해야 합니다.")
         with open('ssd_nand.txt', 'w', encoding='utf-8') as file:
             for line in nand_list:
                 file.write(f"{line}\n")
@@ -25,12 +25,3 @@ class SSDFileManager():
     def print_ssd_output(self, string):
         with open('ssd_output.txt', 'w', encoding='utf-8') as file:
             file.write(string)
-
-
-sfm = SSDFileManager()
-nand = sfm.read_ssd_nand()
-nand[1] = "0x00000001"
-sfm.patch_ssd_nand(nand)
-print(sfm.read_ssd_nand()[1])
-sfm.print_ssd_output("nand")
-
