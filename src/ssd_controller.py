11f39d3bf46d012f61ff77f4edc7315e47ab83df
dong <dla3498204@naver.com>
2025-06-19 16:42:23 +0900

    [feature] fix get read_value from ssd

--- src/ssd_controller.py
+++ src/ssd_controller.py
@@ -3,32 +3,30 @@ import subprocess
 
 
 class SSDController:
-    def write(self, lba_address: str, hex_val: str):
-        result = subprocess.run(
-            ["python", "ssd.py", "W", lba_address, hex_val],
-            capture_output=True,
-            text=True,
-        )
+    def write(self, lba: str, hex_val: str):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        ssd_file_path = os.path.join(script_dir, "ssd.py")
+        subprocess.run(["python", ssd_file_path, "W", lba, hex_val])
         print("[Write] Done")
 
-    def read(self, lba_address: str):
-        env = os.environ.copy()
-        env["SUBPROCESS_CALL"] = "1"  # subprocess 호출임을 알림
+    def read(self, lba: str):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        result = subprocess.run(
-            ["python", "ssd.py", "R", lba_address],
-            capture_output=True,
-            text=True,
-            env=env,
-        )
-        read_value = result.stdout
-        print(f"[Read] LBA {lba_address.zfill(2)} : {read_value}")
+        ssd_file_path = os.path.join(script_dir, "ssd.py")
+        subprocess.run(["python", ssd_file_path, "R", lba])
+
+        output_file_path = os.path.join(script_dir, "ssd_output.txt")
+        with open(output_file_path, "r") as f:
+            read_value = f.read().strip()
+
+        print(f"[Read] LBA {lba.zfill(2)} : {read_value}")
         return read_value
 
     def full_read(self):
-        for lba_address in range(100):
-            self.read(str(lba_address))
+        for lba in range(100):
+            self.read(str(lba))
 
     def full_write(self, hex_val: str):
-        for lba_address in range(100):
-            self.write(str(lba_address), hex_val)
+        for lba in range(100):
+            self.write(str(lba), hex_val)
