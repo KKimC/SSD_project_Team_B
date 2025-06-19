bd02ecc03d48e776c02079142021c860cf081943
taeyun <t109.kang@samsung.com>
2025-06-19 10:34:27 +0900

    [Feature] 공통 상수 별도 파일로 분리

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -6,53 +6,9 @@ from subprocess import CompletedProcess
 import pytest
 from pytest_mock import MockerFixture
 
+from src.constants import HELP_TEXT, INVALID_COMMAND
 from src.ssd_shell import SSDShell
 from src.command import ScriptCommand
-
-INVALID_COMMAND = "INVALID COMMAND"
-HELP_TEXT = """
-AUTHOR
-    비긴어게인 팀 제작자: 김성현, 강태윤, 임동혁, 김남민, 김기웅, 정보람, 김민규
-
-NAME
-    Test Shell - SSD 가상 장치 테스트용 커맨드 라인 셸
-
-SYNOPSIS
-    write [LBA] [VALUE]
-    read [LBA]
-    fullwrite [VALUE]
-    fullread
-    help
-    exit
-
-DESCRIPTION
-
-    write
-        지정한 LBA 주소에 값을 기록합니다.
-        사용법: write [LBA 번호] [저장할 값]
-        예시:  write 3 0xAAAABBBB
-
-    read
-        지정한 LBA 주소에서 값을 읽어 출력합니다.
-        사용법: read [LBA 번호]
-        예시:  read 3
-
-    fullwrite
-        전체 LBA(0~99)에 동일한 값을 기록합니다.
-        사용법: fullwrite [저장할 값]
-        예시:  fullwrite 0xABCDFFFF
-
-    fullread
-        전체 LBA(0~99)에서 값을 읽어 순차적으로 출력합니다.
-        사용법: fullread
-
-    help
-        명령어 목록 및 설명과 제작자 정보를 출력합니다.
-        사용법: help
-
-    exit
-        Test Shell을 종료합니다.
-        사용법: exit"""
 
 
 def _do_run_and_get_result_from_buffer(shell):
@@ -122,7 +78,6 @@ def test_READ_LBA유효성검사_정수가아님(mocker: MockerFixture, shell):
 
     # act and assert
     assert _do_run_and_get_result_from_buffer(shell).strip() == expected.strip()
-
 
 
 def test_WRITE명령어_누락된인자(mocker: MockerFixture, shell):
@@ -433,6 +388,7 @@ def test_WRITE_READ_AGING_실패_FAIL_WRITE_5번(mocker: MockerFixture, shell):
     assert result.replace("\n", "") == "FAIL"
     assert mock_write.call_count == 2
 
+
 def test_FULLREAD_reads를_형식에_맞게_보내는지(mocker: MockerFixture, shell):
     mocker.patch("builtins.input", return_value="fullread")
     expected_line_num = 100
@@ -445,4 +401,3 @@ def test_FULLREAD_reads를_형식에_맞게_보내는지(mocker: MockerFixture, 
         actual_call = mock_subprocess.call_args_list[i]
         actual_args = actual_call.args[0]
         assert actual_args == expected_cmd, f"LBA {i} 에서 명령어 불일치: {actual_args}"
-
