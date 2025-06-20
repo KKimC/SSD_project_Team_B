0f24aaf1db51793174effe61d662fbb2dee42c9a
ken <choirtop@gmail.com>
2025-06-20 16:20:57 +0900

    [refactor] apply strategy pattern for ScriptCommand classes
    src/command.py: 테스트 스크립트 부분 제거
    src/command_script.py: 테스트 스크립트 부분 전략 패턴 적용
    src/command_factory: factory에서 concrete strategy 의존성 제거

---
+++ src/command_script.py
@@ -0,0 +1,124 @@
+import inspect
+from abc import ABC, abstractmethod
+
+from src.constants import TestScriptType, EMPTY_VALUE
+from src.custom_exception import ExitException
+from src.logger import Logger
+from src.ssd_controller import SSDController
+from src.utils.helpers import generate_random_hex
+
+
+logger = Logger()
+
+
+def _read_compare(receiver: SSDController, lba: int, write_value: str):
+    return "PASS" if receiver.read(str(lba)).strip() == write_value.strip() else "FAIL"
+
+
+class ScriptCommandType(ABC):
+    def __init__(self, args, receiver):
+        self.args = args
+        self.receiver = receiver
+
+    @abstractmethod
+    def execute(self): ...
+
+
+class FullWriteAndReadCommand(ScriptCommandType):
+    SCRIPT_TYPE = "1_"
+
+    def execute(self):
+        for i in range(20):
+            write_value_list = [generate_random_hex() for _ in range(5)]
+
+            lba_address = i * 5
+            for value in write_value_list:
+                self.receiver.write(str(lba_address), value)
+                lba_address += 1
+
+            lba_address = i * 5
+            for value in write_value_list:
+                if _read_compare(self.receiver, lba_address, value) == "FAIL":
+                    raise ExitException
+                lba_address += 1
+        logger.print(
+            f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}()",
+            f"SCRIPT_1",
+        )
+
+
+class PartialLbaWriteCommand(ScriptCommandType):
+    SCRIPT_TYPE = "2_"
+
+    def execute(self):
+        for _ in range(30):
+            write_value = generate_random_hex()
+            lba_address_list = [4, 0, 3, 1, 2]
+            for write_lba_address in lba_address_list:
+                self.receiver.write(str(write_lba_address), write_value)
+            for read_lba_address in range(5):
+                if (
+                    _read_compare(self.receiver, read_lba_address, write_value)
+                    == "FAIL"
+                ):
+                    raise ExitException
+        logger.print(
+            f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}()",
+            f"SCRIPT_2",
+        )
+
+
+class WriteReadAgingCommand(ScriptCommandType):
+    SCRIPT_TYPE = "3_"
+
+    def execute(self):
+        lba_address_list = [0, 99]
+        for _ in range(200):
+            write_value_list = [generate_random_hex()] * 2
+            for i, lba_address in enumerate(lba_address_list):
+                self.receiver.write(str(lba_address), write_value_list[i])
+            for i, lba_address in enumerate(lba_address_list):
+                if (
+                    _read_compare(self.receiver, lba_address, write_value_list[i])
+                    == "FAIL"
+                ):
+                    raise ExitException
+        logger.print(
+            f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}()",
+            f"SCRIPT_3",
+        )
+
+
+class EraseAndAgingCommand(ScriptCommandType):
+    SCRIPT_TYPE = "4_"
+
+    def execute(self):
+        for i in range(30):
+            lba_address_list = [2 * (i + 1), 2 * (i + 1) + 1, 2 * (i + 1) + 2]
+            write_value = generate_random_hex()
+            self.receiver.write(str(lba_address_list[0]), write_value)
+            # Overwrite
+            self.receiver.write(str(lba_address_list[0]), write_value)
+            self.receiver.erase(str(lba_address_list[0]), str(3))
+            for lba_address in lba_address_list:
+                if _read_compare(self.receiver, lba_address, EMPTY_VALUE) == "FAIL":
+                    raise ExitException
+        logger.print(
+            f"{self.__class__.__name__}.{inspect.currentframe().f_code.co_name}()",
+            f"SCRIPT_4",
+        )
+
+
+list_script_commands = [
+    FullWriteAndReadCommand,
+    PartialLbaWriteCommand,
+    WriteReadAgingCommand,
+    EraseAndAgingCommand,
+]
+
+
+def get_matched_script(command_str: str):
+    for script_command in list_script_commands:
+        if command_str.startswith(script_command.SCRIPT_TYPE):
+            return script_command
+    return None
