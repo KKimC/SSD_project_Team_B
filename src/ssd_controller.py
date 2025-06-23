d8c8faa5d39fff9d4f021d29adc1c85a54987c08
taeyun <t109.kang@samsung.com>
2025-06-23 13:23:13 +0900

    [Feature] ssd_nand, ssd_output txt 파일 관리 위치 변경

--- src/ssd_controller.py
+++ src/ssd_controller.py
@@ -1,33 +1,26 @@
 import os
 import subprocess
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+SSD_FILE_PATH = os.path.join(SCRIPT_DIR, "ssd.py")
+OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "outputs")
+SSD_OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR, "ssd_output.txt")
+
 
 class SSDController:
     def write(self, lba: str, hex_val: str):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-
-        ssd_file_path = os.path.join(script_dir, "ssd.py")
-        subprocess.run(["python", ssd_file_path, "W", lba, hex_val])
+        subprocess.run(["python", SSD_FILE_PATH, "W", lba, hex_val])
 
     def read(self, lba: str):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
+        subprocess.run(["python", SSD_FILE_PATH, "R", lba])
 
-        ssd_file_path = os.path.join(script_dir, "ssd.py")
-        subprocess.run(["python", ssd_file_path, "R", lba])
-
-        output_file_path = os.path.join(script_dir, "ssd_output.txt")
-        with open(output_file_path, "r") as f:
+        with open(SSD_OUTPUT_FILE_PATH, "r") as f:
             read_value = f.read().strip()
 
         return read_value
 
     def erase(self, lba: str, size: str):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        ssd_file_path = os.path.join(script_dir, "ssd.py")
-
-        subprocess.run(["python", ssd_file_path, "E", lba, size])
+        subprocess.run(["python", SSD_FILE_PATH, "E", lba, size])
 
     def flush(self):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        ssd_file_path = os.path.join(script_dir, "ssd.py")
-        subprocess.run(["python", ssd_file_path, "F"])
+        subprocess.run(["python", SSD_FILE_PATH, "F"])
--- src/ssd_file_manager.py
+++ src/ssd_file_manager.py
@@ -1,39 +1,45 @@
 import os
 
-class SSDFileManager():
+OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "outputs")
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+SSD_NAND_FILE_PATH = os.path.join(OUTPUT_DIR, "ssd_nand.txt")
+SSD_OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR, "ssd_output.txt")
+
+
+class SSDFileManager:
     def __init__(self):
-        if not os.path.exists('ssd_nand.txt'):
-            with open('ssd_nand.txt', 'w', encoding='utf-8') as f:
+        if not os.path.exists(SSD_NAND_FILE_PATH):
+            with open(SSD_NAND_FILE_PATH, "w", encoding="utf-8") as f:
                 f.writelines(["\n"] * 100)
             self.patch_ssd_nand(["0x00000000" for _ in range(100)])
 
-        if not os.path.exists('ssd_output.txt'):
-            with open('ssd_output.txt', 'w', encoding='utf-8') as f:
+        if not os.path.exists(SSD_OUTPUT_FILE_PATH):
+            with open(SSD_OUTPUT_FILE_PATH, "w", encoding="utf-8") as f:
                 f.write("")
 
-        buffer_dir = 'buffer'
+        buffer_dir = "buffer"
         if not os.path.exists(buffer_dir):
             os.makedirs(buffer_dir)
 
         existing_files = os.listdir(buffer_dir)
-        existing_prefixes = {filename.split('_')[0] for filename in existing_files}
+        existing_prefixes = {filename.split("_")[0] for filename in existing_files}
 
         for i in range(1, 6):
             if str(i) not in existing_prefixes:
                 buffer_file = os.path.join(buffer_dir, f"{i}_empty")
-                with open(buffer_file, 'w', encoding='utf-8') as f:
+                with open(buffer_file, "w", encoding="utf-8") as f:
                     f.write("")
 
     def read_ssd_nand(self):
-        with open('ssd_nand.txt', 'r', encoding='utf-8') as file:
+        with open(SSD_NAND_FILE_PATH, "r", encoding="utf-8") as file:
             lines = file.readlines()
-            return [line.rstrip('\n') for line in lines[:100]]
+            return [line.rstrip("\n") for line in lines[:100]]
 
     def patch_ssd_nand(self, nand_list):
-        with open('ssd_nand.txt', 'w', encoding='utf-8') as file:
+        with open(SSD_NAND_FILE_PATH, "w", encoding="utf-8") as file:
             for line in nand_list:
                 file.write(f"{line}\n")
 
     def print_ssd_output(self, string):
-        with open('ssd_output.txt', 'w', encoding='utf-8') as file:
+        with open(SSD_OUTPUT_FILE_PATH, "w", encoding="utf-8") as file:
             file.write(string)
